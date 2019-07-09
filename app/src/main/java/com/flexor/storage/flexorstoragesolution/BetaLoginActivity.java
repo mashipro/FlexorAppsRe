efdc31fa13ffb2568853fa0e428ed5569f4bfe37
Rizki CL <captain.neon99@gmail.com>
2019-07-10 01:56:34 +0700

    WIP BetaLogin and Splashscreen 2.0

--- app/src/main/java/com/flexor/storage/flexorstoragesolution/BetaLoginActivity.java
+++ app/src/main/java/com/flexor/storage/flexorstoragesolution/BetaLoginActivity.java
@@ -5,16 +5,24 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.flexor.storage.flexorstoragesolution.Models.User;
 import com.flexor.storage.flexorstoragesolution.Utility.UserManager;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.firestore.auth.User;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreSettings;
 
 public class BetaLoginActivity extends AppCompatActivity {
     private static final String TAG = "BetaLoginActivity";
@@ -23,6 +31,9 @@ public class BetaLoginActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
     private FirebaseUser firebaseUser;
+    private FirebaseFirestore firebaseFirestore;
+    private FirebaseFirestoreSettings firebaseFirestoreSettings;
+    private DocumentReference userDocument;
 
     /*UI Declare*/
     private Button button_login, button_signup, button_accept;
@@ -30,7 +41,7 @@ public class BetaLoginActivity extends AppCompatActivity {
     private TextView text_forgotpw;
 
     /*Utilities Declare*/
-    private UserManager userManager;
+//    private UserManager userManager;
 
     /*Custom Value Declare*/
     private Boolean flagRegist = false;
@@ -41,14 +52,20 @@ public class BetaLoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_beta_login);
 
-        /*Firebase Init*/
+//        /*Firebase Init*/
+        firebaseFirestore = FirebaseFirestore.getInstance();
+        firebaseFirestoreSettings = new FirebaseFirestoreSettings.Builder()
+                .setPersistenceEnabled(true)
+                .build();
+        firebaseFirestore.setFirestoreSettings(firebaseFirestoreSettings);
+
         mAuth = FirebaseAuth.getInstance();
         firebaseUser = mAuth.getCurrentUser();
         mAuthListener = new FirebaseAuth.AuthStateListener() {
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
                 if (firebaseUser != null){
-                    startActivity(new Intent());
+                    startActivity(new Intent(BetaLoginActivity.this, MainActivity.class));
                 }
             }
         };
@@ -66,7 +83,7 @@ public class BetaLoginActivity extends AppCompatActivity {
         text_forgotpw = findViewById(R.id.link_forgotPW);
 
         /*Utilities Init*/
-        userManager = new UserManager();
+//        userManager = new UserManager();
         checkUser();
 
         initView();
@@ -123,9 +140,137 @@ public class BetaLoginActivity extends AppCompatActivity {
     }
 
     private void emailLogin() {
+        Log.d(TAG, "emailLogin: init");
+        String email = edit_mail.getText().toString().trim();
+        String password = edit_psw.getText().toString().trim();
+        Log.d(TAG, "emailLogin: email= "+ email+" pass= "+password);
+
+        if (email.isEmpty()){
+            edit_mail.setError(getString(R.string.error_emailEmpty));
+            edit_mail.requestFocus();
+            return;
+        }
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            edit_mail.setError(getString(R.string.error_emailInvalid));
+            edit_mail.requestFocus();
+            return;
+        }
+        if (password.isEmpty()){
+            edit_psw.setError(getString(R.string.error_passwordEmpty));
+            edit_psw.requestFocus();
+            return;
+        }
+        if (password.length()<6){
+            edit_psw.setError(getString(R.string.error_passwordTShort));
+            edit_psw.requestFocus();
+            return;
+        }
+        mAuth.signInWithEmailAndPassword(edit_mail.getText().toString(),edit_psw.getText().toString()).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    Log.d(TAG, "onComplete: email sign in success!");
+                    checkEmailVerified();
+                } else {
+                    Log.d(TAG, "onComplete: email sign in failed");
+                    Toast.makeText(BetaLoginActivity.this, getString(R.string.login_email_failed), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private void checkEmailVerified() {
+        Log.d(TAG, "checkEmailVerified: init");
+        FirebaseUser user = mAuth.getCurrentUser();
+        if (user!= null){
+            if (!user.isEmailVerified()){
+                Log.d(TAG, "checkEmailVerified: false");
+                mAuth.signOut();
+                Toast.makeText(this, getString(R.string.error_emailNotVerified), Toast.LENGTH_SHORT).show();
+            }else {
+                Log.d(TAG, "checkEmailVerified: true, go to main!");
+                startActivity(new Intent(BetaLoginActivity.this, BetaMainActivity.class));
+            }
+        }
+    }
+
+    private void sendEmailVerification() {
+        Log.d(TAG, "sendEmailVerification: init");
+        FirebaseUser user = mAuth.getCurrentUser();
+        if (user!= null){
+            if (!user.isEmailVerified()){
+                user.sendEmailVerification().addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()){
+                            Toast.makeText(BetaLoginActivity.this, getString(R.string.success_emailVerificationSent), Toast.LENGTH_SHORT).show();
+                            Log.d(TAG, "onComplete: email sent");
+                        }
+                    }
+                });
+            }
+        }
     }
 
     private void emailRegist() {
+        String email = edit_mail.getText().toString().trim();
+        String password = edit_psw.getText().toString().trim();
+        String name = edit_name.getText().toString().trim();
+        Log.d(TAG, "emailRegist: name= "+name+ " email= "+email+" pass= "+ password);
+
+        if (email.isEmpty()){
+            edit_mail.setError(getString(R.string.error_emailEmpty));
+            edit_mail.requestFocus();
+            return;
+        }
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            edit_mail.setError(getString(R.string.error_emailInvalid));
+            edit_mail.requestFocus();
+            return;
+        }
+        if (password.isEmpty()){
+            edit_psw.setError(getString(R.string.error_passwordEmpty));
+            edit_psw.requestFocus();
+            return;
+        }
+        if (password.length()<6){
+            edit_psw.setError(getString(R.string.error_passwordTShort));
+            edit_psw.requestFocus();
+            return;
+        }
+
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    Log.d(TAG, "onComplete: register success!");
+                    sendEmailVerification();
+                    uploadUserInfo();
+                }
+            }
+        });
+    }
+
+    private void uploadUserInfo() {
+        Log.d(TAG, "uploadUserInfo: init");
+        FirebaseUser firebaseUser = mAuth.getCurrentUser();
+        if (firebaseUser != null){
+            userDocument = firebaseFirestore.collection("Users").document(firebaseUser.getUid());
+            User userDataModel = new User();
+            userDataModel.setUserID(firebaseUser.getUid());
+            userDataModel.setUserEmail(firebaseUser.getEmail());
+            userDataModel.setUserName(edit_name.getText().toString());
+            userDocument.set(userDataModel).addOnCompleteListener(new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+                    if (task.isSuccessful()){
+                        Log.d(TAG, "onComplete: data stored and logging out!");
+                        mAuth.signOut();
+                        startActivity(new Intent(BetaLoginActivity.this, BetaLoginActivity.class).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
+                    }
+                }
+            });
+        }
     }
 
     @Override
---
+++ app/src/main/java/com/flexor/storage/flexorstoragesolution/SplashScreenActivity.java
@@ -0,0 +1,63 @@
+package com.flexor.storage.flexorstoragesolution;
+
+import android.content.Intent;
+import android.os.CountDownTimer;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class SplashScreenActivity extends AppCompatActivity {
+    private static final String TAG = "SplashScreenActivity";
+
+    /*Firebase Declare*/
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private FirebaseUser firebaseUser;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        /*Firebase Init*/
+        mAuth = FirebaseAuth.getInstance();
+        firebaseUser = mAuth.getCurrentUser();
+        Log.d(TAG, "onCreate: userID = "+ firebaseUser);
+        
+        /*usercheck*/
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                if (firebaseUser == null){
+                    startActivity(new Intent(SplashScreenActivity.this, BetaLoginActivity.class).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP));
+                }else {
+                    startActivity(new Intent(SplashScreenActivity.this, BetaMainActivity.class));
+                }
+            }
+        };
+
+        new CountDownTimer(5000, 1000){
+            @Override
+            public void onTick(long millisUntilFinished) {
+
+            }
+
+            @Override
+            public void onFinish() {
+
+            }
+        };
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+}
